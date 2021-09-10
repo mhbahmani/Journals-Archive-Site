d8176e79568eca420eb8b4d35c8d2c7a25ecc75c
mhbahmani <mhbahmani79@gmail.com>
2021-09-10 22:16:44 +0430

    create publication model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from accounts.models import Publisher
+from accounts.models import Publisher, Publication  
 
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
     pass
