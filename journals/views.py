dd5ea4d80907b8e561d48c2d13c0ce9809644f9e
mhbahmani <mhbahmani79@gmail.com>
2021-09-13 17:00:06 +0430

    fix exception import

--- journals/views.py
+++ journals/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, FileResponse
 from django.views.generic import ListView
 from django.shortcuts import render
-from django.db.models import Model
+from django.core.exceptions import MultipleObjectsReturned
 
 from journals.models import Publication
 from accounts.models import Publisher
@@ -29,5 +29,5 @@ def publication_serve_view(request, publisher, publication):
     try:
         publication = Publication.objects.get(title=publication, publisher__title=publisher)
         return FileResponse(publication.pdf)
-    except Model.MultipleObjectsReturned:
+    except MultipleObjectsReturned:
         return render(request, 'message.html', {'message': 'خطایی در یافتن نشریه‌ی مورد نظر به وجود آمده‌است.'})
