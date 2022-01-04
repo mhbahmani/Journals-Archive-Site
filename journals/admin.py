cadd4916c4d5b90a9f4679ecdd48dcc591a45797
mhbahmani <mhbahmani79@gmail.com>
2022-01-04 13:01:07 +0330

    comment list_display_link in publications admin

--- journals/admin.py
+++ journals/admin.py
@@ -6,6 +6,6 @@ from journals.models import Publication
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ['title', 'publisher', 'show']
-    list_display_links = None
+    # list_display_links = None
     list_editable = ['show']
     list_filter = ['publisher']
